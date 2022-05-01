2767fed3b1b0a8ae1df723a0c98b29779a37a711
M4RC0Sx <marcos3as3@gmail.com>
2022-05-01 03:21:45 +0200

    feat: added new settings.

--- src/util/config.ts
+++ src/util/config.ts
@@ -19,6 +19,16 @@ export const ACTIVITY = config ? config.ACTIVITY : process.env.ACTIVITY;
 export const GUILD_ID = config ? config.GUILD_ID : process.env.GUILD_ID;
 export const CLIENT_ID = config ? config.CLIENT_ID : process.env.CLIENT_ID;
 
+export const SQLITE_DATABASE = config
+  ? config.SQLITE_DATABASE
+  : process.env.SQLITE_DATABASE;
+export const SQLITE_USER = config
+  ? config.SQLITE_USER
+  : process.env.SQLITE_USER;
+export const SQLITE_PASSWORD = config
+  ? config.SQLITE_PASSWORD
+  : process.env.SQLITE_PASSWORD;
+
 export const MAIN_COLOR = config ? config.MAIN_COLOR : process.env.MAIN_COLOR;
 
 export const INSTAGRAM_USER = config
@@ -43,6 +53,15 @@ export const ROLE_ADMIN_ID = config
 export const CHANNEL_USER_COUNTER_ID = config
   ? config.CHANNEL_USER_COUNTER_ID
   : process.env.CHANNEL_USER_COUNTER_ID;
+export const CHANNEL_CHERRY_COUNTER_ID = config
+  ? config.CHANNEL_CHERRY_COUNTER_ID
+  : process.env.CHANNEL_CHERRY_COUNTER_ID;
+export const CHANNEL_SUNFLOWER_COUNTER_ID = config
+  ? config.CHANNEL_SUNFLOWER_COUNTER_ID
+  : process.env.CHANNEL_SUNFLOWER_COUNTER_ID;
+export const CHANNEL_ROSE_COUNTER_ID = config
+  ? config.CHANNEL_ROSE_COUNTER_ID
+  : process.env.CHANNEL_ROSE_COUNTER_ID;
 export const CHANNEL_STAFF_LOG_ID = config
   ? config.CHANNEL_STAFF_LOG_ID
   : process.env.CHANNEL_STAFF_LOG_ID;
